7c812d5de9107b39091e511f1d21622d975516f5
carlosarguelles <carlosnapx@gmail.com>
2021-08-03 00:23:45 -0500

    After dead actually 😭 [SOLVED]

--- scripts/gauss_seidel.m
+++ scripts/gauss_seidel.m
@@ -17,6 +17,7 @@ function gauss_seidel(matriz, b, x0, user_error, max_iter)
   x = x0;
 
   % Se inicializa la variable que contiene la suma del método
+  current_sum = 0;
 
   % Se itera hasta alcanzar el máximo de iteraciones
   for i = 1 : max_iter
@@ -26,30 +27,24 @@ function gauss_seidel(matriz, b, x0, user_error, max_iter)
 
       % Se realiza la sumatoria propuesta por el método Gauss_Seidel
 
-     % for k = 1 : j - 1
+      for k = 1 : j - 1
 
-     %   current_sum = current_sum + matriz(j, k) * x(j);
+        current_sum = current_sum + matriz(j, k) * x(k);
 
-     % end
+      end
 
-     % for k = j + 1 : n
-     %   
-     %   current_sum = current_sum + matriz(j, k) * x0(j);
-     % 
-     % end
+      for k = j + 1 : n
+        
+        current_sum = current_sum + matriz(j, k) * x0(k);
+      
+      end
 
-     % x(j) = (b(j) - current_sum) / matriz(j, j);
+      x(j) = (b(j) - current_sum) / matriz(j, j);
 
-     % % Se le asigna 0 a la variable para la próxima iteración
+      % Se le asigna 0 a la variable para la próxima iteración
 
-     % current_sum = 0;
-          new_values_sum = matriz(j, 1 : j - 1) * x(1 : j - 1);
+      current_sum = 0;
 
-          % compute the sum usjng the prevjous step values
-          old_values_sum = matriz(j, j + 1 : n) * x0(j + 1 : n);
-
-          x(j) = b(j) - (old_values_sum + new_values_sum);
-          x(j) = x(j) / matriz(j, j);
     end
 
     % Se verifica el error aproximado
